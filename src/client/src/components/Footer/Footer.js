951ace4c801061de3391eb86955cfc099f2d24ec
qhuongng <nq.huong.2304@gmail.com>
2023-12-10 21:22:29 +0700

    Update header & footer

--- src/client/src/components/Footer/Footer.js
+++ src/client/src/components/Footer/Footer.js
@@ -5,98 +5,82 @@ import withRouter from "../../wrappers/HookWrapper";
 import "./Footer.css";
 
 class Footer extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const location = this.props.location.pathname;
-
-    if (location === "/login" || location === "/signup") {
-      return (
-        <div className="footer">
-          <div className="footer-down">
-            <h4>© Copyright ALT+F4 2023. All rights reserved</h4>
-          </div>
-        </div>
-      );
+    constructor(props) {
+        super(props);
     }
 
-    return (
-      <div className="footer">
-        <div className="footer-up">
-          <div className="footer-content">
-            <div className="footer-subcribe">
-              <Link to="/" className="footer-logo"></Link>
-              <div className="footer-subcribe-more">
-                <h3>227 Nguyen Van Cu Dist 5, HCMC, Vietnam</h3>
-                <h3>altf4.21clc@gmail.com</h3>
-                <h3>+84-12-345678</h3>
-              </div>
-            </div>
-            <div className="footer-each">
-              <h2>Account</h2>
-              <div className="footer-each-more">
-                <div className="footer-page-text">My account</div>
-                <div className="footer-page-text">Cart</div>
-                <Link to="/books" className="footer-page-text">
-                  All books
-                </Link>
-              </div>
-            </div>
+    render() {
+        const location = this.props.location.pathname;
 
-            <div className="footer-each">
-              <h2>Support</h2>
-              <div className="footer-each-more">
-                <div className="footer-page-text">Privacy Policy</div>
-                <div className="footer-page-text">Terms of Use</div>
-                <div className="footer-page-text">FAQ</div>
-                <div className="footer-page-text">Contact</div>
-              </div>
-            </div>
+        if (location === "/login" || location === "/signup") {
+            return (
+                <div className="footer">
+                    <div className="footer-down">
+                        <h4>© Copyright ALT+F4 2023. All rights reserved</h4>
+                    </div>
+                </div>
+            );
+        }
 
-            <div className="footer-follow">
-              <h2>Follow Us</h2>
-              <div className="footer-follow-more">
-                <a
-                  href="https://www.facebook.com/dinhchhoang"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaFacebook className="footer-icon" />
-                </a>
-                <a
-                  href="https://www.facebook.com/dinhchhoang"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaTwitter className="footer-icon" />
-                </a>
-                <a
-                  href="https://www.facebook.com/dinhchhoang"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaInstagram className="footer-icon" />
-                </a>
-                <a
-                  href="https://www.facebook.com/dinhchhoang"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaLinkedin className="footer-icon" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        return (
+            <div className="footer">
+                <div className="footer-up">
+                    <div className="footer-content">
+                        <div className="footer-subcribe">
+                            <Link to="/" className="footer-logo"></Link>
+                            <div className="footer-subcribe-more">
+                                <h3>227 Nguyen Van Cu Dist 5, HCMC, Vietnam</h3>
+                                <h3>altf4.21clc@gmail.com</h3>
+                                <h3>+84-12-345678</h3>
+                            </div>
+                        </div>
+                        <div className="footer-each">
+                            <h2>Account</h2>
+                            <div className="footer-each-more">
+                                <div className="footer-page-text">My account</div>
+                                <div className="footer-page-text">Cart</div>
+                                <Link className="footer-page-text" to="/books">
+                                    All books
+                                </Link>
+                            </div>
+                        </div>
 
-        <div className="footer-down">
-          <h4>© Copyright ALT+F4 2023. All rights reserved</h4>
-        </div>
-      </div>
-    );
-  }
+                        <div className="footer-each">
+                            <h2>Support</h2>
+                            <div className="footer-each-more">
+                                <div className="footer-page-text">Privacy Policy</div>
+                                <div className="footer-page-text">Terms of Use</div>
+                                <div className="footer-page-text">FAQ</div>
+                                <div className="footer-page-text">Contact</div>
+                            </div>
+                        </div>
+
+                        <div className="footer-follow">
+                            <h2>Follow Us</h2>
+                            <div className="footer-follow-more">
+                                <a href="https://www.facebook.com/dinhchhoang" target="_blank" rel="noopener noreferrer">
+                                    <FaFacebook className="footer-icon" />
+                                </a>
+                                <a href="https://www.facebook.com/dinhchhoang" target="_blank" rel="noopener noreferrer">
+                                    <FaTwitter className="footer-icon" />
+                                </a>
+                                <a href="https://www.facebook.com/dinhchhoang" target="_blank" rel="noopener noreferrer">
+                                    <FaInstagram className="footer-icon" />
+                                </a>
+                                <a href="https://www.facebook.com/dinhchhoang" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin className="footer-icon" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer-down">
+                    <h4>© Copyright ALT+F4 2023. All rights reserved</h4>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withRouter(Footer);
