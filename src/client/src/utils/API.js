8edb1f0a60f5e7ff9eed05b03ec239dced5db35a
ngocvu9412 <98596186+ngocvu9412@users.noreply.github.com>
2023-12-19 17:19:31 +0700

    Fix API.js and merge FE in Books.js

--- src/client/src/utils/API.js
+++ src/client/src/utils/API.js
@@ -1,54 +1,51 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:3030';
 
-//Prepared Function
-async function getBooksFromListId(productId) {
+// Prepared Function
+export async function getBooksFromListId(productId) {
   const data = [];
 
   for (const Id of productId) {
-    const result = await exports.getBooksByAttributes({ id: Id });
+    const result = await getBooksByAttributes({ id: Id });
     data[Id] = result[0];
   }
 
   return Object.values(data);
 }
 
-//Read Part
-//Books
-//Home Page
-//Recently Post && You Might Like
-exports.getBooksByPage=(page,limit) =>{
-  return axios.get(`${API_BASE_URL}/products?_page=${page}&_limit=${limit}`)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
-    console.error('Error fetching data:', error);
-  });
-};
-//Done
-//AllBooks
-exports.getAllBooks =() => {
-  return axios.get(`${API_BASE_URL}/products`)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
-    console.error('Error fetching data:', error);
-  });
-};
-//Done
-exports.getBooksByAttributes = ({id,name, price, userId, publishedAt, quantity, status }) => {
+// Read Part
+// Books
+// Home Page
+// Recently Post && You Might Like
+export async function getBooksByPage(page, limit) {
+  return axios
+    .get(`${API_BASE_URL}/products?_page=${page}&_limit=${limit}`)
+    .then((response) => {
+      const data = response.data;
+      return data;
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+}
+// Done
+// AllBooks
+export async function getAllBooks() {
+  return axios
+    .get(`${API_BASE_URL}/products`)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      throw error;
+    });
+}
+// Done
+
+export async function getBooksByAttributes({ id, name, price, userId, publishedAt, quantity, status }) {
   const params = {
     id,
-    'name_like':name,
+    'name_like': name,
     price,
     userId,
     publishedAt,
@@ -57,196 +54,113 @@ exports.getBooksByAttributes = ({id,name, price, userId, publishedAt, quantity, 
   };
 
   Object.keys(params).forEach((key) => params[key] === undefined && delete params[key]);
-  
-  return axios.get(`${API_BASE_URL}/products`,{params:params})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+
+  return axios
+    .get(`${API_BASE_URL}/products`, { params: params })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      throw error;
+    });
+}
+// Done
+// Reviews
+export async function getBookReviews(productId) {
+  return axios.get(`${API_BASE_URL}/reviews`, { params: { productId: productId } })
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error fetching data:', error);
+    throw error;
   });
 };
-//Done
-
-// exports.getAllBooksAscOrder =({nameOrder, priceOrder, userIdOrder, publishedAtOrder, quantityOrder}) => {
-//   const params = {
-//     "name": nameOrder,
-//     "price": priceOrder,
-//     "userId":userIdOrder,
-//     "publishedAt":publishedAtOrder,
-//     "quantity":quantityOrder
-//   };
-
-//   Object.keys(params).forEach((key) => params[key] === undefined && delete params[key]);
-
-//   const _sort = '';
-//   const _order = '';
-
-//   Object.keys(params).forEach((key) => {
-//     _sort += _sort ? `,${key}` : key;
-//     _order += _order ? `,${params[key]}` : params[key];
-//   });
-
-//   console.log(_sort);
-//   console.log(_order);
-
-//   return axios.get(`${API_BASE_URL}/products?_sort=${_sort}&_order=${_order}`);
-// };
-
-//Get Books With id,name,...=?
-
-
-//Users
-exports.getUserByAttributes =({id,name,email,password,address}) => {
+// Done
+// Carts
+export async function getUserProfileByAttributes({id, name, email, password, address}) {
   const params = {
     id,
-    'name_like':name,
-    email,
-    password,
-    'address_like':address
+    'name_like': name,
+    email, 
+    password, 
+    address
   };
 
   Object.keys(params).forEach((key) => params[key] === undefined && delete params[key]);
-  
-  return axios.get(`${API_BASE_URL}/users`,{params:params})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+
+  return axios
+    .get(`${API_BASE_URL}/users`, { params: params })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      throw error;
+    });
+  };
+export async function getUserCart(userId) {
+  return axios.get(`${API_BASE_URL}/carts`, { params: { userId: userId } })
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error fetching data:', error);
+    throw error;
   });
 };
-//Done
-//Reviews
-exports.getBookReviews=(productId) =>{
-  return axios.get(`${API_BASE_URL}/reviews`,{params : {
-    productId: productId
-  }})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+// Done
+export async function getUserCartProfile(userId) {
+  return axios.get(`${API_BASE_URL}/carts`, { params: { userId: userId } })
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error fetching data:', error);
+    throw error;
   });
 };
-//Done
-//Carts
-exports.getUserCart = (userId) =>{
-  return axios.get(`${API_BASE_URL}/carts`,{params: {
-    userId : userId
-  }})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    const result= getBooksFromListId(data[0].productId);
-    return result;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+// Done
+// Orders
+export async function getUsersPurchasedBooks(userId) {
+  return axios.get(`${API_BASE_URL}/orders`, { params: { userId: userId } })
+    .then(response => {
+      const data = response.data;
+
+      const result = getBooksFromListId(data[0].productId);
+      return result;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+};
+// Done
+export async function getUserOrdersProfile(userId) {
+  return axios.get(`${API_BASE_URL}/orders`, { params: { userId: userId } })
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error fetching data:', error);
+    throw error;
   });
 };
-//Done
-exports.getUserCartProfile = (userId) => {
-  return axios.get(`${API_BASE_URL}/carts`,{params: {
-    userId : userId
-  }})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+// Done
+// Favorites
+export async function getUsersFavoritesBooks(userId) {
+  return axios.get(`${API_BASE_URL}/favorites`, { params: { userId: userId } })
+    .then(response => {
+      const data = response.data;
+
+      const result = getBooksFromListId(data[0].productId);
+      return result;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+};
+// Done
+export async function getUserFavoritesProfile(userId) {
+  return axios.get(`${API_BASE_URL}/favorites`, { params: { userId: userId } })
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error fetching data:', error);
+    throw error;
   });
 };
-//Done
-//Orders
-exports.getUsersPurchasedBooks = (userId) => {
-  return axios.get(`${API_BASE_URL}/orders`,{params: {
-    userId : userId
-  }})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    const result= getBooksFromListId(data[0].productId);
-    return result;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
-    console.error('Error fetching data:', error);
-  });
-};
-//Done
-exports.getUserOrdersProfile = (userId) => {
-  return axios.get(`${API_BASE_URL}/orders`,{params: {
-    userId : userId
-  }})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
-    console.error('Error fetching data:', error);
-  });
-};
-//Done
-//Favorites
-exports.getUsersFavoritesBooks = (userId) => {
-  return axios.get(`${API_BASE_URL}/favorites`,{params: {
-    userId : userId
-  }})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    const result= getBooksFromListId(data[0].productId);
-    return result;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
-    console.error('Error fetching data:', error);
-  });
-};
-//Done
-exports.getUserFavoritesProfile = (userId) => {
-  return axios.get(`${API_BASE_URL}/favorites`,{params: {
-    userId : userId
-  }})
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    return data;
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
-    console.error('Error fetching data:', error);
-  });
-};
-//Done
-//Update Part
-//Books
-exports.UpdateBooksByID = ({id,name, price, userId, publishedAt, quantity, status, image }) => {
+// Done
+// Update Part
+// Books
+export async function UpdateBooksByID({ id, name, price, userId, publishedAt, quantity, status, image }) {
   const body = {
     name,
     price,
@@ -258,22 +172,17 @@ exports.UpdateBooksByID = ({id,name, price, userId, publishedAt, quantity, statu
   };
 
   Object.keys(body).forEach((key) => body[key] === undefined && delete body[key]);
-  
-  axios.patch(`${API_BASE_URL}/products/${id}`,body)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    console.log(data);
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+
+  axios.patch(`${API_BASE_URL}/products/${id}`, body)
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error updating data:', error);
+    throw error;
   });
 };
-//Done
-//Users
-exports.UpdateUserProfileByID =({id,name,email,password,address}) => {
+// Done
+// Users
+export async function UpdateUserProfileByID({ id, name, email, password, address }) {
   const body = {
     name,
     email,
@@ -282,116 +191,74 @@ exports.UpdateUserProfileByID =({id,name,email,password,address}) => {
   };
 
   Object.keys(body).forEach((key) => body[key] === undefined && delete body[key]);
-  
-  axios.patch(`${API_BASE_URL}/users/${id}`,body)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    console.log(data);
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+
+  axios.patch(`${API_BASE_URL}/users/${id}`, body)
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error updating data:', error);
+    throw error;
   });
 };
-//Done
-//Reviews
-exports.UpdateUserReviewByID =({id, productId, userId, text }) => {
+// Done
+// Reviews
+export async function UpdateUserReviewByID({ id, productId, userId, text }) {
   const body = {
     productId,
-    userId, 
+    userId,
     text
   };
 
   Object.keys(body).forEach((key) => body[key] === undefined && delete body[key]);
-  
-  axios.patch(`${API_BASE_URL}/reviews/${id}`,body)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    console.log(data);
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+
+  axios.patch(`${API_BASE_URL}/reviews/${id}`, body)
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error updating data:', error);
+    throw error;
   });
 };
-//Done
-//Carts
-exports.UpdateCartsByUserID =async (userId, productId) => {
+// Done
+// Carts
+export async function UpdateCartsByUserID(userId, productId){
   const body = {
     productId
   };
-  const CartInfo= await exports.getUserCartProfile(userId);
-  axios.patch(`${API_BASE_URL}/carts/${CartInfo[0].id}`,body)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-
-    console.log(data);
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+  const CartInfo = await getUserCartProfile(userId);
+  axios.patch(`${API_BASE_URL}/carts/${CartInfo[0].id}`, body)
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error updating data:', error);
+    throw error;
   });
 };
-//Done
-//Orders
-exports.UpdateOrdersByUserID =async (userId, productId) => {
+// Done
+// Orders
+export async function UpdateOrdersByUserID(userId, productId) {
   const body = {
     productId
   };
 
-  const OrderInfo= await exports.getUserOrdersProfile(userId);
-  axios.patch(`${API_BASE_URL}/orders/${OrderInfo[0].id}`,body)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    console.log(data);
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+  const OrderInfo = await getUserOrdersProfile(userId);
+  axios.patch(`${API_BASE_URL}/orders/${OrderInfo[0].id}`, body)
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error updating data:', error);
+    throw error;
   });
 };
-//Done
-//Favorites
-exports.UpdateFavoritesByUserID =async (userId, productId) => {
+// Done
+// Favorites
+export async function UpdateFavoritesByUserID(userId, productId) {
   const body = {
     productId
   };
-  
-  const FavoriteInfo= await exports.getUserCartProfile(userId);
-  axios.patch(`${API_BASE_URL}/favorites/${FavoriteInfo[0].id}`,body)
-  .then(response => {
-    // Dữ liệu từ response.data sẽ chứa thông tin bạn cần
-    const data = response.data;
-    
-    console.log(data);
-  })
-  .catch(error => {
-    // Xử lý lỗi nếu có
+
+  const FavoriteInfo = await getUserCartProfile(userId);
+  axios.patch(`${API_BASE_URL}/favorites/${FavoriteInfo[0].id}`, body)
+  .then((response) => response.data)
+  .catch((error) => {
     console.error('Error updating data:', error);
+    throw error;
   });
 };
-//Done
-// Testing Function
-async function exampleFunction() {
-  const productId =[
-    10,
-    11
-  ]
-  try {
-    await exports.UpdateFavoritesByUserID(0,productId);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-// Check if this file is the entry point and run the exampleFunction
-if (require.main === module) {
-  exampleFunction();
-}
+// Done
